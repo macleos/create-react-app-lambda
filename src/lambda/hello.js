f05784ea63bccffb98151e0e7a5701c494822367
Samuel <samuel@macleod.space>
2018-11-15 03:48:10 +0000

    Python virtualenv

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,11 +1,17 @@
 // show object spread works, i.e. babel works
+var exec = require("child_process").exec;
 const obj = {
-  foo: 'bar'
+  foo: "bar"
 };
 export function handler(event, context, callback) {
-  console.log('queryStringParameters', event.queryStringParameters);
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({ msg: 'Hello, World!', ...obj })
+  console.log("queryStringParameters", event.queryStringParameters);
+  exec('env/bin/python -c "import sys; print sys.version_info"', function(
+    error,
+    stdout
+  ) {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({ msg: "Hello, World!", ...obj })
+    });
   });
 }
